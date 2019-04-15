8efd3404b0c0cd72745efb65fc5a078be63c1853
Mezi04 <Mezi04@live.com>
2019-04-15 14:02:20 +0800

    提交

---
+++ src/main/java/com/wong/leetcode/Q002.java
@@ -0,0 +1,60 @@
+package com.wong.leetcode;
+
+public class Q002 {
+
+	public static class ListNode {
+		public int val;
+		public ListNode next;
+
+		public ListNode(int val) {
+			this.val = val;
+		}
+		public String toString() {
+			ListNode p = this;
+			String str = "";
+			while(p != null) {
+				str = str + String.format("[%d] -> ", p.val);
+				p = p.next;
+			}
+			return str;
+		}
+	}
+
+	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		int i1 = 0;
+		int iIndex = 0;
+		while (l1 != null) {
+			i1 = i1 * 10 + l1.val;
+			l1 = l1.next;
+		}
+		int i2 = 0;
+		while (l2 != null) {
+			i2 = i2 * 10 + l2.val;
+			l2 = l2.next;
+		}
+		int result = i1 + i2;
+		System.out.println(result);
+		ListNode header = new ListNode(result % 10);
+		ListNode p = header;
+		result = result / 10;
+		while(result != 0) {
+			ListNode cNode = new ListNode(result % 10);
+			p.next = cNode;
+			result = result / 10;
+			p = cNode;
+		}
+		return header;
+	}
+
+	public static void main(String[] args) {
+		ListNode l11 = new ListNode(1);
+		ListNode l12 = new ListNode(8);
+//		ListNode l13 = new ListNode(3);
+		ListNode l21 = new ListNode(0);
+//		ListNode l22 = new ListNode(6);
+//		ListNode l23 = new ListNode(4);
+		l11.next = l12;// l12.next = l13;l21.next = l22; l22.next = l23;
+		System.out.println(addTwoNumbers(l11,l21));
+	}
+
+}
---
+++ src/main/java/com/wong/leetcode/Q004.java
@@ -0,0 +1,54 @@
+package com.wong.leetcode;
+
+
+public class Q004 {
+
+	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		if(nums1.length == 0) {
+			return nums2.length % 2 == 0 ? (nums2[nums2.length / 2 - 1] + nums2[nums2.length / 2] ) / 2f : nums2[nums2.length / 2];
+		}else if(nums2.length == 0) {
+			return nums1.length % 2 == 0 ? (nums1[nums1.length / 2 - 1] + nums1[nums1.length / 2] ) / 2f : nums1[nums1.length / 2];
+		}
+		int sum = nums1.length + nums2.length;
+		int point = sum % 2 == 0 ? sum / 2 - 1 : sum / 2;
+		int idx1 = 0;
+		int idx2 = 0;
+		int filtered = 0;
+		boolean isOneFinsh = false;
+		while (filtered < point && !isOneFinsh) {
+			if (nums1[idx1] < nums2[idx2]) {
+				++idx1;
+				isOneFinsh = idx1 >= nums1.length;
+			} else {
+				++idx2;
+				isOneFinsh = idx2 >= nums2.length;
+			}
+			++filtered;
+			System.out.println(String.format("[%d : %d : %d :%d : %b]", filtered, point, idx1, idx2,isOneFinsh));
+		}
+		if (isOneFinsh) {
+			int[] nums = idx1 >= nums1.length ? nums2 : nums1;
+			return sum % 2 == 0 ? (nums[point - filtered] + nums[point - filtered + 1]) / 2f : nums[point - filtered];
+		} else {
+			if(sum % 2 == 0) {
+				if(nums1[idx1] < nums2[idx2] && idx1 < nums1.length - 1 && nums2[idx2] > nums1[idx1+1]) {
+					return (nums1[idx1] + nums1[idx1+1]) /2f;
+				}else if(nums2[idx2] < nums1[idx1] && idx2 < nums2.length -1 && nums1[idx1] > nums2[idx2+1]) {
+					return (nums2[idx2] + nums2[idx2+1]) /2f;
+				}else {
+					return (nums1[idx1] + nums2[idx2]) /2f;
+				}
+			}else {
+				return (nums1[idx1] > nums2[idx2] ? nums2[idx2] : nums1[idx1]);
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		int[] nums1 = new int[] { 1,3};
+		int[] nums2 = new int[] { -1,3};
+		System.out.println(findMedianSortedArrays(nums2, nums1));
+	}
+
+}
