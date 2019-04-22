8776d7d8a70d73e1ae2a243dbc58ab05763794b1
Mezi04 <Mezi04@live.com>
2019-04-22 18:18:48 +0800

    加动态规划

--- src/main/java/com/wong/leetcode/Q005.java
+++ src/main/java/com/wong/leetcode/Q005.java
@@ -19,6 +19,7 @@ public class Q005 {
 		int count = 0;
 		for (int i = 0; i < s.length(); ++i) {
 			for (int j = 0; j < s.length() - i - maxlen; ++j) { // 当i与j直接的字符串长度已小于上一个已验证的子串长度就没必要再比较了
+				System.out.println(String.format("%d,%d,%s,%s", i,j,s.substring(i, s.length() - j - 1),reverse.substring(j, s.length() - i - 1)));
 				if (s.charAt(i) == reverse.charAt(j) //首字符不一样肯定不是回文
 						&& s.substring(i, s.length() - j - 1).equals(reverse.substring(j, s.length() - i - 1))) {
 					if (s.length() - i - j > maxlen) {
@@ -32,11 +33,39 @@ public class Q005 {
 		System.out.println(count);
 		return substr;
 	}
+	
+	 public String longestPalindrome_dp(String s) {
+	       int length = s.length();
+			if (length < 1) {
+				return s;
+			}
+			boolean[][] dp = new boolean[length][length];
+			int maxLength = 1;
+			int begin = 0;
+			for (int j = 0; j < length; j++) {
+				for (int i = 0; i <= j; i++) {
+					if (s.charAt(i) == s.charAt(j)) {
+						if (j - i < 2)
+							dp[i][j] = true;
+						else
+							dp[i][j] = dp[i + 1][j - 1];
+					} else {
+						dp[i][j] = false;
+					}
+					if (dp[i][j] && j - i + 1 > maxLength) {
+						maxLength = j - i + 1;
+						begin = i;
+					}
+				}
+			}
+			return s.substring(begin, begin + maxLength);
+	    }
 
 	public static void main(String[] args) {
 //		String str = "baba";
 //		String str = "add";
-		String str = "whdqcudjpisufnrtsyupwtnnbsvfptrcgvobbjglmpynebblpigaflpbezjvjgbmofejyjssdhbgghgrhzuplbeptpaecfdanhlylgusptlgobkqnulxvnwuzwauewcplnvcwowmbxxnhsdmgxtvbfgnuqdpxennqglgmspbagvmjcmzmbsuacxlqfxjggrwsnbblnnwisvmpwwhomyjylbtedzrptejjsaiqzprnadkjxeqfdpkddmbzokkegtypxaafodjdwirynzurzkjzrkufsokhcdkajwmqvhcbzcnysrbsfxhfvtodqabvbuosxtonbpmgoemcgkudandrioncjigbyizekiakmrfjvezuzddjxqyevyenuebfwugqelxwpirsoyixowcmtgosuggrkdciehktojageynqkazsqxraimeopcsjxcdtzhlbvtlvzytgblwkmbfwmggrkpioeofkrmfdgfwknrbaimhefpzckrzwdvddhdqujffwvtvfyjlimkljrsnnhudyejcrtrwvtsbkxaplchgbikscfcbhovlepdojmqybzhbiionyjxqsmquehkhzdiawfxunguhqhkxqdiiwsbuhosebxrpcstpklukjcsnnzpbylzaoyrmyjatuovmaqiwfdfwyhugbeehdzeozdrvcvghekusiahfxhlzclhbegdnvkzeoafodnqbtanfwixjzirnoaiqamjgkcapeopbzbgtxsjhqurbpbuduqjziznblrhxbydxsmtjdfeefsdfsdfsdfsdfbxbzxcbvcbzxcbxvzxvzxcvcpntijqpkuwmqezkhnkwbvwgnkxmkyhlbfuwaslmjzlhocsgtoujabbexvxweigplmlewumcone";
+		String str = "abacdfgdcaba";
+//		String str = "whdqcudjpisufnrtsyupwtnnbsvfptrcgvobbjglmpynebblpigaflpbezjvjgbmofejyjssdhbgghgrhzuplbeptpaecfdanhlylgusptlgobkqnulxvnwuzwauewcplnvcwowmbxxnhsdmgxtvbfgnuqdpxennqglgmspbagvmjcmzmbsuacxlqfxjggrwsnbblnnwisvmpwwhomyjylbtedzrptejjsaiqzprnadkjxeqfdpkddmbzokkegtypxaafodjdwirynzurzkjzrkufsokhcdkajwmqvhcbzcnysrbsfxhfvtodqabvbuosxtonbpmgoemcgkudandrioncjigbyizekiakmrfjvezuzddjxqyevyenuebfwugqelxwpirsoyixowcmtgosuggrkdciehktojageynqkazsqxraimeopcsjxcdtzhlbvtlvzytgblwkmbfwmggrkpioeofkrmfdgfwknrbaimhefpzckrzwdvddhdqujffwvtvfyjlimkljrsnnhudyejcrtrwvtsbkxaplchgbikscfcbhovlepdojmqybzhbiionyjxqsmquehkhzdiawfxunguhqhkxqdiiwsbuhosebxrpcstpklukjcsnnzpbylzaoyrmyjatuovmaqiwfdfwyhugbeehdzeozdrvcvghekusiahfxhlzclhbegdnvkzeoafodnqbtanfwixjzirnoaiqamjgkcapeopbzbgtxsjhqurbpbuduqjziznblrhxbydxsmtjdfeefsdfsdfsdfsdfbxbzxcbvcbzxcbxvzxvzxcvcpntijqpkuwmqezkhnkwbvwgnkxmkyhlbfuwaslmjzlhocsgtoujabbexvxweigplmlewumcone";
 		System.out.println(longestPalindrome(str));
 	}
 }
