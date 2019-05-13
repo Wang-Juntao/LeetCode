07fe4db46346d88ebc5e7a31749e579f6c6fa720
Mezi04 <Mezi04@live.com>
2019-05-13 17:51:14 +0800

    第10题

--- src/main/java/com/wong/leetcode/Q010.java
+++ src/main/java/com/wong/leetcode/Q010.java
@@ -1,7 +1,12 @@
 package com.wong.leetcode;
 
 public class Q010 {
-	
+	/**
+	 * 第一版 无法解决类似 a*a  .*abc 一类的正则式，会返回错误结果
+	 * @param s
+	 * @param p
+	 * @return
+	 */
 	public static boolean isMatch(String s, String p) {
 		if(p.indexOf("*") < 0 && p.indexOf(".") < 0) {
 			return s.equals(p);
@@ -29,61 +34,46 @@ public class Q010 {
 		return sidx == s.length() && pidx == p.length();
 	}
 	
-	public static boolean isMatch_Right(String s, String p) {
-		if(p.indexOf("*") < 0 && p.indexOf(".") < 0) {
-			return s.equals(p);
+
+	/**
+	 * 	动态规划资料:http://www.sohu.com/a/153858619_466939
+	 * 	关键点：最优子结构、边界、转台转换公式
+	 *  
+	 * @param s
+	 * @param p
+	 * @return
+	 */
+	public static boolean isMatch_Dp(String s, String p) {
+		s = "#" + s;
+		p = "#" + p;
+		boolean[][] dp = new boolean[s.length()][p.length()];
+		dp[0][0] = true;
+		for(int j = 2;j<p.length();++j) {
+			dp[0][j] = p.charAt(j)=='*' && dp[0][j-2];
 		}
-		if(p.equals(".*")) {
-			return true;
-		}
-		int pidx = p.length() - 1;
-		int sidx = s.length() - 1;
-		while (pidx >= 0 && sidx >= 0) {
-			char cp = p.charAt(pidx);
-			if(cp =='*') {
-				--pidx;
-				cp = p.charAt(pidx);
-				while (sidx >=0 && (s.charAt(sidx) == cp || cp == '.')) {
-					--sidx;
+		for(int i=1;i<s.length();++i) {
+			for(int j=1;j<p.length();++j) {
+				if(p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {
+					dp[i][j] = dp[i-1][j-1];
+				}else if(p.charAt(j) == '*') {
+					if(p.charAt(j-1) == '.' || s.charAt(i) == p.charAt(j-1)) {
+						dp[i][j] = dp[i][j-1] || dp[i-1][j] || (j>=2 && dp[i][j-2]);
+					}else {
+						dp[i][j] = dp[i][j-2];
+					}
 				}
-			}else if (cp == '.' || s.charAt(sidx) == p.charAt(pidx)) {
-				--sidx;
-				--pidx;
-			} else {
-				return false;
 			}
 		}
-		return sidx == -1 && pidx == -1;
+//		for(boolean[] line : dp) {
+//			for(boolean b : line) {
+//				System.out.print(b+"\t");
+//			}
+//			System.out.println();
+//		}
+		return dp[s.length()-1][p.length()-1];
 	}
 	
-	public static boolean isMatch_(String s, String p) {
-		if(p.indexOf("*") < 0 && p.indexOf(".") < 0) {
-			return s.equals(p);
-		}
-		if(p.equals(".*")) {
-			return true;
-		}
-		
-		
-		int pidx = p.length() - 1;
-		int sidx = s.length() - 1;
-		while (pidx >= 0 && sidx >= 0) {
-			char cp = p.charAt(pidx);
-			if(cp =='*') {
-				--pidx;
-				cp = p.charAt(pidx);
-				while (sidx >=0 && (s.charAt(sidx) == cp || cp == '.')) {
-					--sidx;
-				}
-			}else if (cp == '.' || s.charAt(sidx) == p.charAt(pidx)) {
-				--sidx;
-				--pidx;
-			} else {
-				return false;
-			}
-		}
-		return sidx == -1 && pidx == -1;
-	}
+
 	
 	public static boolean isMatch_sys(String s, String p) {
 		return s.matches(p);
@@ -91,10 +81,10 @@ public class Q010 {
 	
 	
 	public static void main(String[] args) {
-		System.out.println(isMatch("dfsdfdsfsdabc",".*abc"));
-		System.out.println(isMatch_sys("aa","a*aaaa*"));
-		System.out.println(isMatch_sys("dfsdfdsfsdabb",".*abc"));
-		System.out.println(isMatch_sys("dfsdfdsfsdabc",".*abc"));
+//		System.out.println(isMatch_Dp("dfsdfdsfsdabc",".*a*abc"));
+//		System.out.println(isMatch_Dp("aa","a*aaa*"));
+		System.out.println(isMatch_Dp("abde","a*b*c*d*e*"));
+		System.out.println(isMatch_sys("",".*abc"));
 	}
 
 }
